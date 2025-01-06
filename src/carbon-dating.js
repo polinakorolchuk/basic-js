daa7e22a438dbd2d0b5c1fd865b246c9d2a3e3af
Polina <146344200+polinakorolchuk@users.noreply.github.com>
2025-01-06 19:33:28 +0300

    Update carbon-dating.js

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,16 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  if (typeof sampleActivity != 'string') {
+    return false;
+  }
+  const a = Number(sampleActivity);
+  if (isFinite(sampleActivity) && a == sampleActivity &&
+     a <= MODERN_ACTIVITY && a > 0) {
+      return Math.ceil(Math.log(15 / sampleActivity) / (0.693 / HALF_LIFE_PERIOD));
+     }
+     return false;
 }
 
 module.exports = {
